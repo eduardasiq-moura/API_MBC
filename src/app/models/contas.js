89b70ed0b7b95439d3f8ac49d4c36fdb70575ca0
eduardasiq-moura <eduardasiq.moura@gmail.com>
2025-04-09 11:06:57 -0300

    Estruturando 1/4

--- src/app/models/contas.js
+++ src/app/models/contas.js
@@ -1,32 +1,21 @@
 import Sequelize, { Model } from 'sequelize';
-import bcrypt from 'bcryptjs';
 
-class User extends Model {
+class Contas extends Model {
   static init(sequelize) {
     super.init(
       {
-        name: Sequelize.STRING,
-        email: Sequelize.STRING,
-        password: Sequelize.VIRTUAL,
-        password_hash: Sequelize.STRING,
+        nome: Sequelize.STRING,
+        codigo: Sequelize.INTEGER,
       },
       {
         sequelize,
+        tableName: 'contas',
+        timestamps: false,
       }
     );
 
-    this.addHook('beforeSave', async user => {
-      if (user.password) {
-        user.password_hash = await bcrypt.hash(user.password, 8);
-      }
-    });
-
     return this;
-  }
-
-  checkPassword(password) {
-    return bcrypt.compare(password, this.password_hash);
   }
 }
 
-export default User;
+export default Contas;
